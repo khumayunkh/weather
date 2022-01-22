ee1dfda990b628871640f3bf318ed14cb1aad526
khumayunkh <94608144+khumayunkh@users.noreply.github.com>
2022-01-23 00:10:43 +0500

    weather

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
+  const [weather, setWeather]= useState({})
+  const {register, handleSubmit, reset}= useForm()
+  const nameOfCity = register('city')
+  const onSubmit = async (data)=>{
+    const payload ={
+      q: data.city,
+      appid: '64511737548f80e4e4992aac79e10b3a'
+    }
+    const response = await axios.get('https://api.openweathermap.org/data/2.5/weather', {params:payload})
+    console.log(response.data)
+    setWeather(response.data)
+    
+    reset()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="header">
+      <form  onSubmit={handleSubmit(onSubmit)}>
+        <div className='form'>
+          <input {...nameOfCity} className='input' placeholder='Name of city'/>
+           <button className='button'>
+            Go
+           </button>
+       </div>
+      </form>
     </div>
+    <div className='mainm'>
+     <div className='main'>
+       <div className='count'>{weather?.name}</div>
+       <div className='temp'>{weather?.main?.temp_max} Fahrenheit!</div>
+       <div>
+            <div className="my-1"> <img className="my-1" src="https://i.imgur.com/Qw7npIg.png" /> </div>
+            
+       </div>
+     </div>
+    </div>
+    </>
   );
 }
 
